bde19ea9ae71740a101a7fe7ef85c7863fc729d5
belkovskiy <79059926+belkovskiy@users.noreply.github.com>
2021-03-09 22:39:57 +0300

    fix: create the new code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(!array || array.length == 0) {
+		return 0;
+	}
+
+	array.sort(function(a,b) {
+      return a-b;
+    });
+
+	return array[0];
+  //return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+	if(!array || array.length ==0) {
+		return 0;
+	}
+
+	array.sort((a,b) => a-b);
+
+	return array[array.length - 1];
+  //return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(!array || array.length == 0) {
+		return 0;
+	}
+
+	const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+	return array.reduce(reducer) / array.length;
+  //return 0;
 }
